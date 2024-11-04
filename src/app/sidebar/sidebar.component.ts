07fa7eef3668128491cd1c7b1eab51a7a87b7f45
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-03 17:05:07 -0700

    Add initial styles for sidebar component and sidebar header

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,93 @@
 import { Component } from '@angular/core';
 
+const sidebarDataArray = [
+  {
+    title: 'Dashboard',
+    selections: [
+      {
+        selection: 'Home',
+        image: 'fa-solid fa-warehouse',
+        path: 'home',
+      },
+    ],
+  },
+  {
+    title: 'Pages',
+    selections: [
+      {
+        selection: 'Tasks',
+        image: 'fa-solid fa-money-check-dollar',
+        path: 'tasks',
+      },
+      {
+        selection: 'Tenants',
+        image: 'fa-solid fa-user-tie',
+        path: 'tenants',
+      },
+      {
+        selection: 'Orders',
+        image: 'fa-solid fa-bag-shopping',
+        path: 'orders',
+      },
+    ],
+  },
+  {
+    title: 'Apps',
+    selections: [
+      {
+        selection: 'Calender',
+        image: 'fa-solid fa-calendar-days',
+        path: 'apps',
+      },
+      {
+        selection: 'Themes',
+        image: 'fa-solid fa-palette',
+        path: 'themes',
+      },
+      {
+        selection: 'Editor',
+        image: 'fa-solid fa-pen-to-square',
+        path: 'editor',
+      },
+    ],
+  },
+  {
+    title: 'Charts',
+    selections: [
+      {
+        selection: 'Line Chart',
+        image: 'fa-solid fa-chart-line',
+        path: 'line-chart',
+      },
+      {
+        selection: 'Area Chart',
+        image: 'fa-solid fa-chart-area',
+        path: 'area-chart',
+      },
+      {
+        selection: 'Bar Chart',
+        image: 'fa-solid fa-chart-simple',
+        path: 'bar-chart',
+      },
+    ],
+  },
+  {
+    title: 'Support',
+    selections: [
+      {
+        selection: 'Live Chat',
+        image: 'fa-solid fa-headset',
+        path: 'live-chat',
+      },
+      {
+        selection: 'Email',
+        image: 'fa-solid fa-envelope',
+        path: 'email',
+      },
+    ],
+  },
+];
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -7,4 +95,6 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  sidebarData = sidebarDataArray;
+}
