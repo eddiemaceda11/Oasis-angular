f6acd3d5d28f3201c413c5dc2dd08e2af9e45ca0
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-14 22:03:14 -0700

    Import MainComponent into app.component.ts imports array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
