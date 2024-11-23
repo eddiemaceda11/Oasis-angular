d4e6aa789fdc81ace07b2a463d50c9c154ab1efa
Eddie Maceda <eddiemaceda11@gmail.com>
2024-11-23 09:53:30 -0700

    Added the FormsModule to the app.component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, MainComponent],
+  imports: [SidebarComponent, MainComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
